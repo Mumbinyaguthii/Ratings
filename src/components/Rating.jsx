cc8af58634278a6e38f11ef4cf065863b40fe524
mumbinyaguthii <janenyaguthii931@gmail.com>
2026-07-10 10:21:20 +0300

    added hover and feedback

--- src/components/Rating.jsx
+++ src/components/Rating.jsx
@@ -1,15 +1,30 @@
+import { useState } from "react";
+
 const Rating = () => {
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+
   const stars = Array.from({ length: 5 }, (_, index) => index + 1);
+
+  const feedbackMessages = ["Terrible", "Poor", "Fair", "Good", "Excellent"];
+
   return (
     <div className="rating-container">
       <h2>Rate Your Experience</h2>
       <div className="stars">
-        {stars.map((star) => (
-          <span key={star} className="star">
+        {stars.map((star, index) => (
+          <span
+            onClick={() => setRating(star)}
+            onMouseEnter={() => setHover(star)}
+            onMouseLeave={() => setHover(0)}
+            key={star}
+            className={`star ${star <= (hover || rating) ? "active" : ""}`}
+          >
             &#9733;
           </span>
         ))}
       </div>
+      {rating > 0 && <p className="feedback">{feedbackMessages[rating - 1]}</p>}
     </div>
   );
 };
